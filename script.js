72601018ca8512e1a87edafc6f42bd135c844aa3
Ryan Doherty <rjd.atx@gmail.com>
2020-09-03 17:51:30 -0500

    restyle + css

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-window.addEventListener("load", function(){
-    let json = [];
-    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
-        response.json().then(function(json) {
-            json.sort(function(a, b) {
-                return a.hoursInSpace < b.hoursInSpace ? 1 : -1
-            });
-            const container = document.getElementById("container");
-            container.innerHTML += `
+window.addEventListener('load', function () {
+  let json = [];
+  fetch(
+    'https://handlers.education.launchcode.org/static/astronauts.json'
+  ).then(function (response) {
+    response.json().then(function (json) {
+      json.sort(function (a, b) {
+        return a.hoursInSpace < b.hoursInSpace ? 1 : -1;
+      });
+      const container = document.getElementById('container');
+      container.innerHTML += `
             <div>
                 <h2>Crew Counter: ${json.length}</h2>
             </div>
             `;
-            index = 0;
-            while(index < json.length)
-            {
-                let color = 'red';
-                if(json[index].active === true)
-                {
-                    color = 'green';
-                }
-                container.innerHTML += `
-                    <div class="astronaut">
-                        <div class="bio">
-                            <h3>${json[index].firstName} ${json[index].lastName}</h3>
-                            <ul>
-                                <li>Hours in space: ${json[index].hoursInSpace}</li>
-                                <li style="color:${color}">Active: ${json[index].active}</li>
-                                <li>Skills: ${json[index].skills.join(', ')}</li>
+      index = 0;
+      while (index < json.length) {
+        let color = 'red';
+        if (json[index].active === true) {
+          color = 'green';
+        }
+        container.innerHTML += `
+                    <div class="card">
+                        <img style=width:100% src="${json[index].picture}"/>
+                            <button>${json[index].firstName} ${
+          json[index].lastName
+        }</button>
+                            <ul class="title">
+                                <li>Hours in space: ${
+                                  json[index].hoursInSpace
+                                }</li>
+                                <li style="color:${color}">Active: ${
+          json[index].active
+        }</li>
+                                <li>Skills: ${json[index].skills.join(
+                                  ', '
+                                )}</li>
                             </ul>
                         </div>
-                        <img class="avatar" src="${json[index].picture}"/>
                     </div>
                     `;
-                index++;
-            }
-        });
-    });    
- });
+        index++;
+      }
+    });
+  });
+});
